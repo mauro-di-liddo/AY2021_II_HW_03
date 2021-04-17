25f37aab80d4095845466ceeb106c49532cba38c
Davide <10531186@polimi.it>
2021-04-17 16:58:50 +0200

    primar program structure created

---
+++ GROUP_03.cydsn/InterruptRoutines.c
@@ -0,0 +1,20 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "InterruptRoutines.h"
+#include "stdio.h"
+#include "project.h"
+
+CY_ISR(Custom_ISR_timer)  //timer overflow handler (useful for RX timeouts)
+{
+    Timer_ReadStatusRegister();  //clears interrupt flag 
+}
+/* [] END OF FILE */
--- GROUP_03.cydsn/main.c
+++ GROUP_03.cydsn/main.c
@@ -10,13 +10,18 @@
  * ========================================
 */
 #include "project.h"
+#include "InterruptRoutines.h"
+#include "stdio.h"
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+       ADC_DelSig_Start(); 
+       Timer_Start();
+       isr_timer_StartEx(Custom_ISR_timer);
+       ADC_DelSig_StartConvert();
     for(;;)
     {
         /* Place your application code here. */
