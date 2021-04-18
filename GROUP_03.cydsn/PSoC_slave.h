cd0185551e51d115882a00febaee2f11fd6f3872
Davide <10531186@polimi.it>
2021-04-18 18:36:36 +0200

    added header file

---
+++ GROUP_03.cydsn/PSoC_slave.h
@@ -0,0 +1,18 @@
+
+
+#ifndef __PSoC_H
+    #define __PSoC_H
+
+    #define PSoC_DEVICE_ADDRESS 0x08
+    #define PSoC_WHO_AM_I_REG_ADDR 0x02
+    #define PSoC_ADD_REG1 0x00
+    #define PSoC_ADD_REG2 0x01
+    
+    #define OFF_MODE 0x14
+    #define PHT_MODE 0x15
+    #define TEMP_MODE 0x16
+    #define CONTEMP_MODE 0x17
+    
+#endif
+
+/* [] END OF FILE */
