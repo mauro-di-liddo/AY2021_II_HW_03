2e4be345c153532f34015dbab74c44a62653a754
Davide <10531186@polimi.it>
2021-04-18 09:05:03 +0200

    set up of the WHO AM I register

--- GROUP_03.cydsn/main.c
+++ GROUP_03.cydsn/main.c
@@ -13,15 +13,29 @@
 #include "InterruptRoutines.h"
 #include "stdio.h"
 
+#define SLAVE_BUFFER_SIZE 7
+
+uint8_t slaveBuffer[SLAVE_BUFFER_SIZE];
+uint8_t Control_Reg_1;
+uint8_t Control_Reg_2;
+
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-       ADC_DelSig_Start(); 
-       Timer_Start();
-       isr_1_StartEx(Custom_ISR_timer);
-       ADC_DelSig_StartConvert();
+        ADC_DelSig_Start(); 
+        Timer_Start();
+        isr_1_StartEx(Custom_ISR_timer);
+        EZI2C_1_Start();
+        ADC_DelSig_StartConvert();
+    
+        slaveBuffer[0] = Control_Reg_1;
+        slaveBuffer[1] = Control_Reg_2;
+        // Set up who am i register
+        slaveBuffer[SLAVE_BUFFER_SIZE-5] = 0xBC;
+    
+       
     for(;;)
     {
         /* Place your application code here. */
