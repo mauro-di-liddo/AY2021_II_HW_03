dc2bad389065e5c34e5e5917292c7dd2277e7bb4
Davide <10531186@polimi.it>
2021-04-21 10:54:30 +0200

    changed PSoC_slave.h variable value and added comment

--- GROUP_03.cydsn/PSoC_slave.h
+++ GROUP_03.cydsn/PSoC_slave.h
@@ -3,8 +3,8 @@
 
     #define PSoC_DEVICE_ADDRESS 0x08
     #define PSoC_WHO_AM_I_REG_ADDR 0x02
-    #define PSoC_ADD_REG1 0x00
-    #define PSoC_ADD_REG2 0x01
+    #define PSoC_ADD_REG1 0b00111100 //da capire cosa mettere di default 
+    #define PSoC_ADD_REG2 0b00000000 //da capire cosa mettere di default
 
     #define OFF_MODE 0b00
     #define PHT_MODE 0b0
